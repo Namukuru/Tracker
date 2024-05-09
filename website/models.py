e2d1f1989e77d32ecef859acc071adf2893a3b19
Namukuru Lauryn <namukurulauryn@gmail.com>
2024-05-09 14:47:33 +0300

    Creation of the budget model

--- website/models.py
+++ website/models.py
@@ -42,3 +42,14 @@ class Income(models.Model):
 
     def __str__(self):
         return (f"{self.user} - {self.amount} - {self.date}")
+
+
+class Budget(models.Model):
+    id = models.AutoField(primary_key=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return (f"{self.user} - {self.amount} - {self.date}")
